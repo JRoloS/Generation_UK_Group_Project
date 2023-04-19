3ff265746eb193d9f78ab2c4e9ff06777541b3c7
Sadat A <sadateawuma@gmail.com>
2023-04-19 14:35:25 +0100

    Created extra folder for extra documents

--- source/connection.py
+++ source/connection.py
@@ -1,20 +1,23 @@
-import pymysql
+# import pymysql
 import psycopg2
 import os
 from dotenv import load_dotenv
 
+# Loads in .env which contains db credentials:
 load_dotenv()
 host_name = os.environ.get("sql_host")
 database_name = os.environ.get("sql_db")
 user_name = os.environ.get("sql_user")
 user_password = os.environ.get("sql_pass")
 
+
+# Does what it says on the tin mate:
 def setup_db_connection(host=host_name, 
                         user=user_name, 
                         password=user_password,
                         db=database_name):
 
-    connection = pymysql.connect(
+    connection = psycopg2.connect(
         host = host,
         database = db,
         user = user,
